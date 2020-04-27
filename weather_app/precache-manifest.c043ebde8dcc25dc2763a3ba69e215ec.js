1c81a21e8b546cc0087957f4e8c8d1ca6ebc4962
EliasLip <elia0649@kea.stud.com>
2020-04-27 10:20:09 +0200

    auto-commit

---
+++ weather_app/precache-manifest.c043ebde8dcc25dc2763a3ba69e215ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7c128c1cbaa89383f0ccd00e277a74",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "c1796635345bd64a5020",
+    "url": "/weather_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7d232f04f4b78393e3b",
+    "url": "/weather_app/static/js/2.cbd46d96.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weather_app/static/js/2.cbd46d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1796635345bd64a5020",
+    "url": "/weather_app/static/js/main.90782274.chunk.js"
+  },
+  {
+    "revision": "591e2505f6ee8d973312",
+    "url": "/weather_app/static/js/runtime-main.207e6037.js"
+  }
+]);
